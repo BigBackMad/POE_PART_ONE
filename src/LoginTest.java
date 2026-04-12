ebfdb141b17639d244e44e5f4ac84637c569676e
BigBackMad <ST10517344@rcconnect.edu.za>
2026-04-12 19:51:33 +0200

    init commit

--- src/LoginTest.java
+++ src/LoginTest.java
@@ -8,12 +8,8 @@ public class LoginTest {
     public void testCheckUsername(){
 
         Login login = new Login();
-        String username = login.getUserName();
 
-        username ="kyl_1";
         assertTrue(login.checkUserName("kyl_1"));
-
-        username ="kyle!!!!!!!";
         assertFalse(login.checkUserName("kyle!!!!!!!"));
     }
 
@@ -21,12 +17,8 @@ public class LoginTest {
     public void testCheckPasswordComplexity(){
 
         Login login = new Login();
-        String password = login.getPassword();
 
-        password = "Ch&&sec@ke99!";
         assertTrue(login.checkPasswordComplexity("Ch&&sec@ke99!"));
-
-        password = "password";
         assertFalse(login.checkPasswordComplexity("password"));
     }
 
@@ -34,12 +26,8 @@ public class LoginTest {
     public void testCheckCellPhoneNumber(){
 
         Login login = new Login();
-        String phoneNumber = login.getPhoneNumber();
 
-        phoneNumber = "+27838968976";
         assertTrue(login.checkCellPhoneNumber("+27838968976"));
-
-        phoneNumber = "08966553";
         assertFalse(login.checkCellPhoneNumber("08966553"));
 
 
