800dc739e778bc2112279e4b46c3d67af09a045a
BigBackMad <ST10517344@rcconnect.edu.za>
2026-04-13 20:51:08 +0200

    update

--- src/LoginTest.java
+++ src/LoginTest.java
@@ -6,59 +6,38 @@ public class LoginTest {
 
     @Test
     public void testCheckUsernameTrue(){
-
         Login login = new Login();
-        String username = login.getUserName();
-        username = "kyl_1";
-        String expected = "--Username successfully captured!--\n";
-        String actual = login.registerUser();
-        assertEquals(expected, actual);
+        assertTrue(login.checkUserName("kyl_1"));
     }
 
     @Test
     public void testCheckUsernameFalse(){
-
         Login login = new Login();
-        String expected = "kyle!!!!!!!";
-        String actual = login.getUserName();
-        assertEquals(expected, "Username is not correctly formatted", actual);
+        assertFalse(login.checkUserName("kyle!!!!!!"));
     }
 
     @Test
     public void testCheckPasswordComplexityTrue(){
-
         Login login = new Login();
-        String expected = "Ch&&sec@ke99!";
-        String actual = login.getPassword();
-        assertEquals(expected, "Password is correctly formatted", actual);
+        assertTrue(login.checkPasswordComplexity("Ch&&sec@ke99!"));
     }
 
     @Test
     public void testCheckPasswordComplexityFalse(){
-
         Login login = new Login();
-        String expected = "password";
-        String actual = login.getPassword();
-        assertEquals(expected, "Password is not correctly formatted", actual);
+        assertFalse(login.checkPasswordComplexity("password"));
     }
 
     @Test
     public void testCheckCellPhoneNumberTrue(){
-
         Login login = new Login();
-        String expected = "+27838968976";
-        String actual = login.getPhoneNumber();
-        assertEquals(expected, "Phone number is correct", actual);
+        assertTrue(login.checkCellPhoneNumber("+27838968976"));
     }
 
     @Test
     public void testCheckCellPhoneNumberFalse(){
         Login login = new Login();
-        String expected = "08966553";
-        String actual = login.getPhoneNumber();
-        assertEquals(expected, "Phone number is not correct", actual);
+        assertFalse(login.checkCellPhoneNumber("08966553"));
     }
-
-
 
 }
