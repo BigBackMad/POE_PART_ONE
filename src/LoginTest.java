02b910f6930e8d4ed6364a84f23abdb67898f0fe
BigBackMad <ST10517344@rcconnect.edu.za>
2026-04-13 14:50:13 +0200

    update test class

--- src/LoginTest.java
+++ src/LoginTest.java
@@ -5,33 +5,49 @@ public class LoginTest {
 
 
     @Test
-    public void testCheckUsername(){
+    public void testCheckUsernameTrue(){
 
         Login login = new Login();
-
         assertTrue(login.checkUserName("kyl_1"));
+    }
+
+    @Test
+    public void testCheckUsernameFalse(){
+
+        Login login = new Login();
         assertFalse(login.checkUserName("kyle!!!!!!!"));
     }
 
     @Test
-    public void testCheckPasswordComplexity(){
+    public void testCheckPasswordComplexityTrue(){
 
         Login login = new Login();
-
         assertTrue(login.checkPasswordComplexity("Ch&&sec@ke99!"));
-        assertFalse(login.checkPasswordComplexity("password"));
     }
 
     @Test
-    public void testCheckCellPhoneNumber(){
+    public void testCheckPasswordComplexityFalse(){
 
         Login login = new Login();
-
-        assertTrue(login.checkCellPhoneNumber("+27838968976"));
-        assertFalse(login.checkCellPhoneNumber("08966553"));
-
+        assertFalse(login.checkPasswordComplexity("password"));
 
     }
+
+    @Test
+    public void testCheckCellPhoneNumberTrue(){
+
+        Login login = new Login();
+        assertTrue(login.checkCellPhoneNumber("+27838968976"));
+    }
+
+    @Test
+    public void testCheckCellPhoneNumberFalse(){
+        Login login = new Login();
+        assertFalse(login.checkCellPhoneNumber("08966553"));
+    }
+
+    @Test
+
 
 
 }
